0fe355cbdf3b6506d3c7236844dbaa60d69fbf3b
whitehorse66 <mkuwana@keio.jp>
2021-02-09 17:00:39 +0900

    first commit

---
+++ examples/opengl3/Window.h
@@ -0,0 +1,108 @@
+#pragma once
+#include<iostream>
+#include <GL/gl3w.h> 
+#include <GLFW/glfw3.h>
+
+//ウィンドウ関連の処理
+class Window {
+	//ウィンドウのハンドル
+	GLFWwindow* const window;
+
+	//ウィンドウのサイズ
+	GLfloat size[2];
+
+	//ワールド座標系に対するデバイス座標系の拡大率
+	GLfloat scale;
+
+	//図形の正規化デバイス座標系上での位置
+	GLfloat location[2];
+
+
+
+public:
+	//コンストラクタ
+	Window(int width = 1280, int height = 720, const char* title = "Window") :
+		window(glfwCreateWindow(width, height, title, NULL, NULL)) 
+		,scale(100.0f)
+	{
+
+		if (window == NULL)
+		{
+			//ウィンドウが作成できなかった
+			std::cerr << "Can't create GLFW window." << std::endl;
+			exit(1);
+		}
+
+		//現在のウィンドウを処理対象にする関数
+		glfwMakeContextCurrent(window);
+
+		//GLEWを初期化する
+		if (gl3wInit()) {
+			//GLEWの初期化に失敗した
+			std::cerr << "Can't initialize GLEW" << std::endl;
+			exit(1);
+		}
+
+		//垂直同期のタイミングを待つ
+		//カラーバッファの入れ替えのタイミングを指定する
+		glfwSwapInterval(1);
+
+		//ウィンドウサイズ変更時に呼び出す処理の登録
+		glfwSetWindowSizeCallback(window, resize);
+
+		//このインスタンスのthisポインタを記録しておく
+		glfwSetWindowUserPointer(window, this);
+
+		//開いたウィンドウの初期設定
+		resize(window, width, height);
+	}
+
+	//デストラクタ
+	virtual ~Window() {
+		glfwDestroyWindow(window);
+	}
+
+	//描画ループの継続判定
+	explicit operator bool() {
+		//イベントを取り出す
+		glfwWaitEvents();
+
+		//ウィンドウを閉じる必要がなければtrueを返す
+		return !glfwWindowShouldClose(window);
+	}
+
+	//ダブルバッファリング
+	void swapBuffers() const {
+		//カラーバッファを入れ替える
+		glfwSwapBuffers(window);
+	}
+
+	//ウィンドウのサイズ変更時の処理
+	static void resize(GLFWwindow* const window, int width, int height) {
+		//フレームバッファのサイズを調べる
+		int fbWidth, fbHeight;
+		glfwGetFramebufferSize(window, &fbWidth, &fbHeight);
+
+		//フレームバッファ全体をビューポートに設定する
+		glViewport(0, 0, fbWidth, fbHeight);
+
+		//このインスタンスのthisポインタを得る
+		Window* const
+			instance(static_cast<Window*>(glfwGetWindowUserPointer(window)));
+		if (instance != NULL) {
+			
+			//開いたウィンドウのサイズを保存する
+			instance->size[0] = static_cast<GLfloat>(width);
+			instance->size[1] = static_cast<GLfloat>(height);
+		}
+	}
+
+	//ウィンドウのサイズを取り出す
+	const GLfloat* getSize() const { return size; }
+
+	//ワールド座標系に対するデバイス座標系の拡大率を取り出す
+	GLfloat getScale() const { return scale; }
+
+	//windowを取り出す
+	GLFWwindow* getWindow() const { return window; }
+};
---
+++ examples/opengl3/object.h
@@ -0,0 +1,67 @@
+#pragma once
+#include <GL/gl3w.h>
+
+
+//図形データ
+class Object {
+	//頂点配列オブジェクト名
+	GLuint vao;
+	
+	//頂点バッファオブジェクト名
+	GLuint vbo;
+
+public:
+	//頂点属性
+	struct Vertex {
+		//位置
+		GLfloat position[2];
+	};
+
+
+
+	//コンストラクタ
+	//size:頂点位置の次元
+	//vertexcount:頂点の数
+	//vertex:頂点属性を格納した配列
+	Object(GLint size,GLsizei vertexcount, const Vertex* vertex) {
+		//頂点配列オブジェクトを作成
+		glGenVertexArrays(1, &vao);
+		//頂点配列オブジェクトを結合
+		glBindVertexArray(vao);
+
+		//頂点バッファオブジェクトを作成
+		glGenBuffers(1, &vbo);
+		//頂点バッファオブジェクトを結合
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		//頂点バッファオブジェクトにデータ (頂点属性) を転送する
+		glBufferData(GL_ARRAY_BUFFER, vertexcount * sizeof(Vertex), vertex, GL_STATIC_DRAW);
+
+		//結合されている頂点バッファオブジェクトをin変数から参照できるようにする
+		//頂点バッファオブジェクトをattribute変数に関連づける
+		glVertexAttribPointer(0, size, GL_FLOAT, GL_FALSE, 0, 0);
+		//Attribute変数を有効にする
+		glEnableVertexAttribArray(0);
+	}
+
+	//デストラクタ
+	virtual ~Object() {
+		//頂点配列オブジェクトを削除する
+		glDeleteVertexArrays(1, &vao);
+
+		//頂点バッファオブジェクトを削除する
+		glDeleteBuffers(1, &vbo);
+	}
+private:
+	//コピーコンストラクタによるコピー禁止
+	Object(const Object& o);
+
+	//代入によるコピー禁止
+	Object& operator=(const Object& o);
+
+public:
+	//頂点配列オブジェクトの結合
+	void bind()const {
+		//描画する頂点配列オブジェクトを指定する
+		glBindVertexArray(vao);
+	}
+};
